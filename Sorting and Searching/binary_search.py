2b4b62bb29c8e00368b0c88ed175dcb1d8e1b9bb
Alex Waweru <njorogealexw@gmail.com>
2019-01-09 07:53:11 -0800

    search and sorting

--- Sorting and Searching/binary_search.py
+++ Sorting and Searching/binary_search.py
@@ -14,9 +14,34 @@ def binarySearch(alist, item):
     
     return found
 
+
+def recursiveBinarySearch(alist, item):
+    first = 0
+    last  = len(alist) - 1
+    if last >= first:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            return True
+        elif alist[midpoint] > item:
+            return recursiveBinarySearch(alist[:midpoint-1], item)
+        else:
+            return recursiveBinarySearch(alist[midpoint+1:], item)
+    else:
+        return False
+
+
+
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
 print(binarySearch(testlist, 3))
 print(binarySearch(testlist, 13))
+
+# Output:
+# False
+# True
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(recursiveBinarySearch(testlist, 3))
+print(recursiveBinarySearch(testlist, 13))
 
 # Output:
 # False
--- Stacks and Queues/question2.py
+++ Stacks and Queues/question2.py
@@ -17,5 +17,29 @@ class Stack:
      def size(self):
          return len(self.items)
     
-     def min(self):
-         
+     def min_element(self):
+         min_el = self.items[0]
+         pos = 1
+         while pos <= self.size()-1:
+             if self.items[pos] < min_el:
+                 min_el = self.items[pos]
+             else:
+                 pos = pos + 1
+        
+         return min_el
+
+
+new_stack = Stack()
+new_stack.push(100)
+new_stack.push(101)
+new_stack.push(145)
+new_stack.push(19)
+new_stack.push(1789)
+new_stack.push(190)
+new_stack.push(23)
+new_stack.push(10)
+new_stack.push(1001)
+print(new_stack.min_element())
+
+# Output:
+# 10
